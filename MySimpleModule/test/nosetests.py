f5120e39d8dfaa1146721479ebb4663974a9d8b9
Tim Seed <tim@sy-edm.com>
2018-04-06 10:52:31 +0400

    Extra test

--- MySimpleModule/test/nosetests.py
+++ MySimpleModule/test/nosetests.py
@@ -18,5 +18,11 @@ def test_020_isin_bad():
     global cls
     assert cls.isInData(99) == False
 
+def test_030_isin_bad_data():
+    global cls
+    try:
+        cls.isInData("a")
+    except Exception as err:
+        assert type(err) == ValueError
 
 
