3c31eeccd8d2c481b1732ff7dc30e44a1f69269b
Tim Seed <tim@sy-edm.com>
2018-05-12 07:35:07 +0400

    Removed __init__.py as tests as this was causing a nose error

--- MySimpleModule/test/test_002.py
+++ MySimpleModule/test/test_002.py
@@ -9,32 +9,12 @@ def test_000_init():
     assert cls
 
 
-def test_010_isin_good():
-    global cls
-    assert cls.isInData(1) == True
-
-
-def test_020_isin_bad():
-    global cls
-    assert cls.isInData(99) == False
-
 def test_030_isin_bad_data():
     global cls
     try:
         cls.isInData("a")
     except Exception as err:
         assert type(err) == ValueError
-
-
-
-def test_gen_lots():
-    global cls
-    for n in range(1,4):
-        assert check_this(n) == True
-    for n in range(4, 8):
-        assert check_this(n) == False
-
-
 
 class report(object):
     def __call__(self, ExpectedResult,desc,value,n):
